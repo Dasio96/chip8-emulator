6cc46fcfd496634e1d671630304b6e04cee8e45f
Dasio96 <dawid01852@gmail.com>
2026-08-21 17:51:40 +0200

    feat: implement load byte (6XNN) and add byte (7XNN)

--- src/main.rs
+++ src/main.rs
@@ -115,6 +115,18 @@ impl Chip8 {
                 }
             }
 
+            op if (op & 0xF000) == 0x6000 => {
+                let x = ((op & 0x0F00) >> 8) as usize;
+                let nn = (op & 0x00FF) as u8;
+                self.registers[x] = nn;
+            }
+
+            op if (op & 0xF000) == 0x7000 => {
+                let x = ((op & 0x0F00) >> 8) as usize;
+                let nn = (op & 0x00FF) as u8;
+                self.registers[x] = self.registers[x].wrapping_add(nn);
+            }
+
             _ => println!("{:#06X}", opcode),
         }
     }
